661cb3e31f772a892d30861939708e6a8344b2f2
Kate Gordon <kategordon.code@gmail.com>
2019-10-24 10:40:36 -0400

    Wine MVC

---
+++ models/wineModel.js
@@ -0,0 +1,21 @@
+const db = require('./conn.js');
+
+class WineList {
+    constructor(id, producer, region, country, varietals, year, type){
+        this.id = id;
+        this.producer = producer;
+        this.region = region;
+        this.country = country; 
+        this.varietals = varietals; 
+        this.year = year; 
+        this.type = type; 
+    }
+
+    static async getAllWineData() {
+        try {
+            const response = await db.any(`SELECT * FROM wines`);  
+            return response;
+        } catch(err) {
+            return err.message;
+        }
+    }
