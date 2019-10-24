c32a43bce26dab379297d7b211208c09c5cc044b
Felipe Dunbar <felipe.a.dunbar@gmail.com>
2019-10-24 11:43:25 -0400

    Added user login and signup

--- models/userModel.js
+++ models/userModel.js
@@ -2,22 +2,37 @@ const db = require("./conn");
 bcrypt = require("bcryptjs");
 
 class User {
-    constructor (first_name, last_name, email_address, password, company) {
+    constructor (first_name, last_name, email_address, password, company, role) {
         this.first_name = first_name;
         this.last_name = last_name;
         this.email_address = email_address;
         this.password = password;
         this.company = company;
+        this.role = role;
+    }
+
+    checkPassword(hashedPassword) {
+        return bcrypt.compareSync(this.password, hashedPassword);
     }
 
     async login() {
         try {
             const response = await db.one(
-                `SELECT first_name, last_name, password, company 
-                    FROM user 
+                `SELECT first_name, last_name, password, company, role 
+                    FROM users 
                     WHERE email = $1`, 
             [this.email_address]);
             console.log("response is: ", response);
+
+            const isValid = this.checkPassword(response.password);
+
+            if (!!isValid) {
+                const { id, first_name, last_name } = response;
+                return { isValid, id, first_name, last_name };
+            } else {
+                return { isValid };
+            }
+            
         } catch(err) {
             return err.message;
         }
@@ -25,7 +40,7 @@ class User {
 
     async save() {
         try {
-            const response = await db.one(`INSERT INTO user (first_name, last_name, email, password, company) VALUES ($1, $2, $3, $4, $5) RETURNING id;`, [this.first_name, this.last_name, this.last_name, this.email_address, this.password, this.company]);
+            const response = await db.one(`INSERT INTO user (first_name, last_name, email, password, company, role) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id;`, [this.first_name, this.last_name, this.last_name, this.email_address, this.password, this.company]);
         return response;
         }catch(err) {
             return err.message;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express'),
  router = express.Router();
 
+const UserModel = require("../models/userModel");
+
   router.get('/', async (req, res, next) => {
     res.render("template", {
       locals: {
@@ -26,6 +28,43 @@ const express = require('express'),
         partial: "partial-signup"
       }
     });
+  });
+
+  
+  router.post("/signup", async (req, res, next) => {
+    const { first_name, last_name, email_address, company, role } = req.body;
+  
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+  
+    const buyer = new UserModel(first_name, last_name, email_address, hash, company, role);
+  
+    const addBuyer = await buyer.save();
+  
+    if (addBuyer) { 
+      res.status(200).redirect("/");
+    } else {
+      res.status(500);
+    }
+  });
+  
+  router.post("/", async (req, res, next) => {
+    const { email_address, password } =req.body;
+  
+    const buyer = new UserModel(null, null, email_address, password, null, null, null);
+  
+    const response = await buyer.login();
+  
+    if (!! response.isValid) {
+      const { id, first_name, last_name } = response;
+      req.session.is_logged_in = true;
+      req.session.first_name = first_name;
+      req.session.last_name = last_name;
+      req.session.user_id = id;
+      res.status(200).redirect("/");
+    } else {
+      res.sendStatus(401);
+    }
   });
 
 module.exports = router;
