1329d1318cea438f02d6132f6654ca1ad6b2eac0
Kate Gordon <kategordon.code@gmail.com>
2019-10-25 10:38:17 -0400

    changed route, model, and views

---
+++ routes/buyerList.js
@@ -0,0 +1,34 @@
+const express = require('express'),
+  router = express.Router();
+
+  const wineListModel = require("../models/wineListModel");
+
+  router.get('/', async (req, res, next) => {
+    const userListData= await wineListModel.userList();
+    res.render("template", {
+      locals: {
+        title: "User Wine List",
+        listData: userListData  
+      },
+      partials: {
+        partial: "partial-buyer"
+      }
+    });
+  });
+
+  router.get("/", async (req, res, next) => {
+    const wineUser = await WineUserModel.userList();
+
+    res.render("template", {
+        locals: {
+            title: "Wine User",
+            wineData: wineUser
+        },
+        partials: {
+            partial: "partial-buyer"
+        }
+    });
+});
+  module.exports = router;
+
+  
