8b6f2fd7ff90959cb64f76b30ab0d307c1bab5bf
Felipe Dunbar <felipe.a.dunbar@gmail.com>
2019-10-28 09:40:37 -0400

    Removed console logs

--- routes/buyerList.js
+++ routes/buyerList.js
@@ -9,10 +9,10 @@ const express = require('express'),
   router.get('/:user_id', async (req, res, next) => {
     const { user_id } = req.params;
     const buyer = await userModel.getById(user_id);
-    console.log('buyer is: ', buyer)
+    
 
     const userListData = await userListModel.myList(user_id);
-    console.log("User Data: ", userListData);
+    
     res.status(200).render("template", {
       locals: {
         title: "My Wine List",
