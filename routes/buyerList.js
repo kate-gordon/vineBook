f91f832e439c816f40106404c04c69ab745681ab
Kate Gordon <kategordon.code@gmail.com>
2019-10-28 11:47:43 -0400

    Filtered arrays for buyer list and single buyer view

--- routes/buyerList.js
+++ routes/buyerList.js
@@ -2,22 +2,24 @@ const express = require('express'),
   router = express.Router();
 
   const userListModel = require("../models/userListModel");
-  const userModel = require("../models/userModel");
-
 
   /// Route to My User List
 
   router.get('/:user_id', async (req, res, next) => {
     const { user_id } = req.params;
-    const buyer = await userModel.getById(user_id);
-    
 
     const userListData = await userListModel.myList(user_id);
+
+    const invData = userListData.filter(list => list.list_type === "Have"); 
+    const needData = userListData.filter(list => list.list_type === "Need");
+
+
     
     res.status(200).render("template", {
       locals: {
         title: "My Wine List",
-        Data: buyer, userListData,
+        haveData: invData, 
+        needData: needData, 
         is_logged_in: req.session.is_logged_in,
         id: req.session.user_id
       },
